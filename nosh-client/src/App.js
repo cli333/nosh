19f3784a1065ab9d9642885fe7828cbbf49ae787
cli333 <cml8198@protonmail.com>
2020-02-17 14:11:26 -0800

    added backend

--- nosh-client/src/App.js
+++ nosh-client/src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import firebase from "./firebase/firebase";
+
+import axios from "axios";
 
 function App() {
+  let food = "1_large_banana";
+
+  useEffect(() => {
+    (async () => {
+      await axios.get(`http://localhost:5000/${food}`);
+      let foodRef = await firebase
+        .firestore()
+        .collection("noshes")
+        .doc(food);
+      foodRef.get().then(res => console.log(res.data()));
+    })();
+    // axios
+    //   .get("http://localhost:5000/1_cup_sugar")
+    //   .then(() => {
+    //   });
+  }, [food]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello world
+      <div>
+        <h1>Testing here</h1>
+      </div>
     </div>
   );
 }
--- nosh-server/server.js
+++ nosh-server/server.js
@@ -2,19 +2,47 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const { firebase } = require("./firebase/firebase");
+
 const axios = require("axios");
 const { edaAppKey, edaAppId } = require("./apis/edamam");
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/:food", async (req, res) => {
-  let food = req.params.food;
-  let url = `https://api.edamam.com/api/nutrition-data?app_id=${edaAppId}&app_key=${edaAppKey}&ingr=1%20large%20${food}`;
+  let food = req.params.food.replace(/_/g, "%20");
+  let url = `https://api.edamam.com/api/nutrition-data?app_id=${edaAppId}&app_key=${edaAppKey}&ingr=${food}`;
   try {
     let result = await axios.get(url);
-    console.log(url, result.data);
-    res.send("console logged ${food}");
+    const {
+      calories,
+      dietLabels,
+      healthLabels,
+      cautions,
+      totalNutrients
+    } = result.data;
+    firebase
+      .firestore()
+      .collection("noshes")
+      .doc(req.params.food)
+      .set({
+        calories,
+        dietLabels,
+        healthLabels,
+        cautions,
+        totalNutrients
+      });
+    res.send(result.data);
   } catch (error) {
     console.log(error);
-    res.send("error finding ${food}");
+    res.send("error");
   }
 });
 
